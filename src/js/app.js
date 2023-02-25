cf4230886d7abf97db67967a611fe4bf82d71dff
Артём Ситников <sitnikov.artem91@gmail.com>
2023-02-25 20:29:19 +0200

    add video__size for screen more then 1200px

--- src/js/app.js
+++ src/js/app.js
@@ -2,12 +2,15 @@ import * as flsFunctions from "./modules/functions.js";
 
 flsFunctions.isWebp();
 
-$('.galary-accessories').slick({
+$('.galary-accessories').slick(
+    {
     slidesToShow: 2,
     slidesToScroll: 2,
     autoplay: true,
     autoplaySpeed: 2000,
   });
+
+
   const btn = document.querySelector('.navigat__btn');
   const menu = document.querySelector('.navbar__menu');
   const lines =document.querySelector('.lines');
